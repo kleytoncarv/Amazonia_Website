5dac15cb6e2664ff5be303d2b3f938da60e07f5f
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2024-11-08 16:22:50 -0300

    imports

---
+++ files/amazonia_historia/src/components/logo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../css/logo.css'
+
+
+function Logo(){
+
+    return (
+ 
+        <div>
+          <h2 className="logo">Logo</h2>
+          
+        </div> 
+  
+    )
+}
+
+export default Logo
