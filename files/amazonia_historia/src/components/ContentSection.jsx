38755d26ce47f0980d1c71dfef97862b5984bf08
Kleyton Carvalho <135996798+kleytoncarv@users.noreply.github.com>
2024-11-11 15:29:59 -0300

    favicon

--- files/amazonia_historia/src/components/ContentSection.jsx
+++ files/amazonia_historia/src/components/ContentSection.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 function ContentSection() {
   return (
     <section className="sec">
-      <h2>História</h2>
+      <h2>História da Amazônia: Uma Viagem Pelo Passado e Presente
+      </h2>
       <p>
         {}
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae porro dolores nobis error mollitia labore nesciunt, odio non rem corporis deleniti, praesentium tempore at incidunt harum totam magnam ab dicta.
+        
       </p>
     </section>
   );
